578a49a0b54edb086e801062da7e71564931cd19
Jachimowski-Pawel <73522925+Jachimowski-Pawel@users.noreply.github.com>
2020-10-30 14:37:08 +0100

    Add files via upload

--- index.php
+++ index.php
@@ -1,3 +1,27 @@
 <?php
-echo("<h1>Paweł Jachimowski Kl.2ti</h1>");
+    echo("<h1>Paweł Jachimowski</h1>");
+    function worker($nr_zadania,$z_sql){
+        $conn=new mysqli("localhost","pawel","z_pawel","pawel");
+        $sql=$z_sql;
+        echo("<h2>Tabelka $nr_zadania </h2>".$sql);
+        $result=$conn->query($sql);
+        echo("<table border=1>");
+        echo("<th>id_pracownicy</th>");
+        echo("<th>imie</th>");
+        echo("<th>dział</th>");
+        echo("<th>zarobki</th>");
+            while($row=$result->fetch_assoc()){
+                echo("<tr>");
+                    echo("<td>".$row["id_pracownicy"]."</td><td>".$row["imie"]."</td><td>".$row['dzial']."</td><td>".$row["zarobki"]."</td>");
+                echo("</tr>");
+            }
+        echo("</table>");
+    }
+worker(1,"SELECT*FROM pracownicy");
+worker(2,"SELECT*FROM pracownicy where imie like'%a'");
+worker(3,"SELECT*FROM pracownicy where imie not like '%a'");
+worker(4,"SELECT*FROM pracownicy where zarobki<30");
+worker(5,"SELECT*FROM pracownicy where dzial=2 or dzial=1");
+    
 ?>
+
