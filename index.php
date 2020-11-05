03d64b506a3f03ce4893f98e798e28e581eefbce
Jachimowski-Pawel <73522925+Jachimowski-Pawel@users.noreply.github.com>
2020-11-05 11:21:43 +0100

    Update index.php

--- index.php
+++ index.php
@@ -31,6 +31,7 @@ worker(2,"SELECT*FROM pracownicy where imie like'%a'","stwórz 5 tabel");
 worker(3,"SELECT*FROM pracownicy where imie not like '%a'","stwórz 5 tabel");
 worker(4,"SELECT*FROM pracownicy where zarobki<30","stwórz 5 tabel");
 worker(5,"SELECT*FROM pracownicy where dzial=2 or dzial=1","stwórz 5 tabel");
+worker(6,"SELECT count(imie) as imie *FROM pracownicy","ilośc pracowników");
 ?>
 
  </body>
