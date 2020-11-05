03b4e15219f0a18c374792054d81d519c2fb3d76
Jachimowski-Pawel <73522925+Jachimowski-Pawel@users.noreply.github.com>
2020-11-05 11:10:24 +0100

    Update index.php

--- index.php
+++ index.php
@@ -31,7 +31,6 @@ worker(2,"SELECT*FROM pracownicy where imie like'%a'","stwórz 5 tabel");
 worker(3,"SELECT*FROM pracownicy where imie not like '%a'","stwórz 5 tabel");
 worker(4,"SELECT*FROM pracownicy where zarobki<30","stwórz 5 tabel");
 worker(5,"SELECT*FROM pracownicy where dzial=2 or dzial=1","stwórz 5 tabel");
-echo("<h1>Paweł Jachimowski Klasa 2Ti Nr.10</h1>");
 function worker($nr_zadania,$z_sql,$polecenie){
     require("connect.php");
     $sql=$z_sql;
@@ -50,7 +49,7 @@ function worker($nr_zadania,$z_sql,$polecenie){
         }
     echo("</table>");
 }
-worker(1,"SELECT*FROM pracownicy","stwórz 5 tabel");
+
 ?>
 
  </body>
