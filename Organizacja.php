b16231d52c84745a1a0ac901e6cae6284774d3a3
Jachimowski-Pawel <73522925+Jachimowski-Pawel@users.noreply.github.com>
2020-11-06 14:18:36 +0100

    Update Organizacja.php

--- Organizacja.php
+++ Organizacja.php
@@ -32,7 +32,7 @@ worker(1,"SELECT imie, dzial, zarobki, nazwa_dzial, zarobki FROM pracownicy, org
 worker(2,"SELECT imie, dzial, zarobki, nazwa_dzial, zarobki FROM pracownicy, organizacja WHERE dzial = id_org and dzial=1 or dzial=4","Pracownicy z działu 1 i 4");
 worker(3,"SELECT imie, dzial, zarobki, nazwa_dzial, zarobki FROM pracownicy, organizacja WHERE dzial = id_org and imie like '%a'","Kobiety z nazwą działu");
 worker(4,"SELECT imie, dzial, zarobki, nazwa_dzial, zarobki FROM pracownicy, organizacja WHERE dzial = id_org and imie not like '%a'","Męzczyźni z nazwą działu");
-worker(5,"SELECT imie, dzial, zarobki, nazwa_dzial, zarobki FROM pracownicy, organizacja WHERE imie desc","Pracownicy posortowani malejąco wg imienia");
+worker(5,"SELECT *FROM pracownicy, organizacja WHERE id_org=dzial order by imie desc","Pracownicy posortowani malejąco wg imienia");
 ?>
 
  </body>
