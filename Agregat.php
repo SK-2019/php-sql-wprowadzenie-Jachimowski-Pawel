b17f730b9e5a9ded7adc04e7cf9816b77efd17c4
Jachimowski-Pawel <73522925+Jachimowski-Pawel@users.noreply.github.com>
2020-11-06 14:59:44 +0100

    Update Agregat.php

--- Agregat.php
+++ Agregat.php
@@ -32,8 +32,9 @@ worker(3,"SELECT SUM(zarobki) as suma_zarobki FROM pracownicy where imie not lik
 worker(4,"SELECT AVG(zarobki) as srednia_zarobki FROM pracownicy where imie not like '%a'","Średnia zarobków wszystkich mężczyzn","srednia_zarobki","-brak danych-");
 worker(5,"SELECT AVG(zarobki) as srednia_zarobki FROM pracownicy where dzial=4","Średnia zarobków z działu 4","srednia_zarobki","-brak danych-");
 worker(6,"SELECT AVG(zarobki) as srednia_zarobki FROM pracownicy where imie not like '%a' and dzial=2 or dzial=3","Średnia zarobków mężczyzn z działu 2 i 3","srednia_zarobki","-brak danych-");
-worker(7,"SELECT COUNT(zarobki) as ilu_pracownicy FROM pracownicy","ilość wszystkich pracowników","ilu_pracownicy","-brak danych-");
-worker(8,"SELECT COUNT(zarobki) as ilu_pracownicy FROM pracownicy where imie like '%a' and dzial=1 or dzial=3","ilość wszystkich pracowników","ilu_pracownicy","-brak danych-");
+worker(7,"SELECT COUNT(imie) as ilu_pracownicy FROM pracownicy","ilość wszystkich pracowników","ilu_pracownicy","-brak danych-");
+worker(8,"SELECT COUNT(imie) as ilu_pracownicy FROM pracownicy where imie like '%a' and dzial=1 or dzial=3","ilość wszystkich pracowników","ilu_pracownicy","-brak danych-");
+worker(9,"SELECT SUM(zarobki) as suma_zarobki FROM pracownicy GROUP BY dzial","ilość wszystkich pracowników","suma_zarobki","dzial");
 ?>
 
     </body>
