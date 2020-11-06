b3886a824d7a63d77cbed4f25a23e9e143af4df9
Jachimowski-Pawel <73522925+Jachimowski-Pawel@users.noreply.github.com>
2020-11-06 14:57:20 +0100

    Update Agregat.php

--- Agregat.php
+++ Agregat.php
@@ -32,6 +32,8 @@ worker(3,"SELECT SUM(zarobki) as suma_zarobki FROM pracownicy where imie not lik
 worker(4,"SELECT AVG(zarobki) as srednia_zarobki FROM pracownicy where imie not like '%a'","Średnia zarobków wszystkich mężczyzn","srednia_zarobki","-brak danych-");
 worker(5,"SELECT AVG(zarobki) as srednia_zarobki FROM pracownicy where dzial=4","Średnia zarobków z działu 4","srednia_zarobki","-brak danych-");
 worker(6,"SELECT AVG(zarobki) as srednia_zarobki FROM pracownicy where imie not like '%a' and dzial=2 or dzial=3","Średnia zarobków mężczyzn z działu 2 i 3","srednia_zarobki","-brak danych-");
+worker(7,"SELECT COUNT(zarobki) as ilu_pracownicy FROM pracownicy","ilość wszystkich pracowników","ilu_pracownicy","-brak danych-");
+worker(8,"SELECT COUNT(zarobki) as ilu_pracownicy FROM pracownicy where imie like '%a' and dzial=1 or dzial=3","ilość wszystkich pracowników","ilu_pracownicy","-brak danych-");
 ?>
 
     </body>
