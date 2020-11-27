12caebba864b1dc115ab22e5a6e579205cc22d57
Jachimowski-Pawel <73522925+Jachimowski-Pawel@users.noreply.github.com>
2020-11-27 15:40:59 +0100

    add: other workers

--- FAgregat.php
+++ FAgregat.php
@@ -36,19 +36,19 @@
                             echo("<th>$kolumna1</th>");
                              while($row=$result->fetch_assoc()){
                                 echo("<tr>");
-                                     echo("<td>".$row[$kolumna1]."td>");
+                                     echo("<td>".$row[$kolumna1]."</td>");
                                 echo("</tr>");
                         };
                             echo("</table>");
                     };
                     worker("Zad 1","SELECT SUM(zarobki) as suma_zarobki FROM pracownicy","Suma zarobków wszystkich pracowników","suma_zarobki");
-                    // worker("Zad 2","SELECT SUM(zarobki) as suma_zarobki FROM pracownicy where imie like '%a'","Suma zarobków kobiet","suma_zarobki");
-                    // worker("Zad 3","SELECT SUM(zarobki) as suma_zarobki FROM pracownicy where imie not like '%a' and dzial=2 or dzial=3","Suma zarobków mężczyzn z działu 2 i 3","suma_zarobki");
-                    // worker("Zad 4","SELECT AVG(zarobki) as srednia_zarobki FROM pracownicy where imie not like '%a'","Średnia zarobków wszystkich mężczyzn","srednia_zarobki");
-                    // worker("Zad 5","SELECT AVG(zarobki) as srednia_zarobki FROM pracownicy where dzial=4","Średnia zarobków z działu 4","srednia_zarobki");
-                    // worker("Zad 6","SELECT AVG(zarobki) as srednia_zarobki FROM pracownicy where imie not like '%a' and dzial=2 or dzial=3","Średnia zarobków mężczyzn z działu 2 i 3","srednia_zarobki");
-                    // worker("Zad 7","SELECT COUNT(imie) as ilu_pracownicy FROM pracownicy","ilość wszystkich pracowników","ilu_pracownicy");
-                    // worker("Zad 8","SELECT COUNT(imie) as ilu_pracownicy FROM pracownicy where imie like '%a' and dzial=1 or dzial=3","ilość wszystkich pracowników","ilu_pracownicy",);
+                    worker("Zad 2","SELECT SUM(zarobki) as suma_zarobki FROM pracownicy where imie like '%a'","Suma zarobków kobiet","suma_zarobki");
+                    worker("Zad 3","SELECT SUM(zarobki) as suma_zarobki FROM pracownicy where imie not like '%a' and dzial=2 or dzial=3","Suma zarobków mężczyzn z działu 2 i 3","suma_zarobki");
+                    worker("Zad 4","SELECT AVG(zarobki) as srednia_zarobki FROM pracownicy where imie not like '%a'","Średnia zarobków wszystkich mężczyzn","srednia_zarobki");
+                    worker("Zad 5","SELECT AVG(zarobki) as srednia_zarobki FROM pracownicy where dzial=4","Średnia zarobków z działu 4","srednia_zarobki");
+                    worker("Zad 6","SELECT AVG(zarobki) as srednia_zarobki FROM pracownicy where imie not like '%a' and dzial=2 or dzial=3","Średnia zarobków mężczyzn z działu 2 i 3","srednia_zarobki");
+                    worker("Zad 7","SELECT COUNT(imie) as ilu_pracownicy FROM pracownicy","ilość wszystkich pracowników","ilu_pracownicy");
+                    worker("Zad 8","SELECT COUNT(imie) as ilu_pracownicy FROM pracownicy where imie like '%a' and dzial=1 or dzial=3","ilość wszystkich pracowników","ilu_pracownicy",);
 
                 ?>
         </body>
