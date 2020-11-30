79102f8364d982652689eeefced43100d7d2e456
Jachimowski-Pawel <73522925+Jachimowski-Pawel@users.noreply.github.com>
2020-11-30 09:16:10 +0100

    Update dataiczas.php

--- dataiczas.php
+++ dataiczas.php
@@ -1,6 +1,6 @@
 <html>
     <head>
-        <link rel="stylesheet" href="style2.css">
+        <link rel="stylesheet" href="style1.css">
     </head>
         <body>
             <div class="nav">
@@ -10,6 +10,23 @@
             <a href="FAgregat.php">Funkcje Agregujace</a>;
             </div>
                 <?php
+                    require("connect.php");
+                    $sql="SELECT *, YEAR(curdate())-YEAR(data_urodzenia) as wiek FROM pracownicy, organizacja WHERE dzial=id_org";
+                    echo("<h2>ZADANIE 1: $sql</h2>");
+                    $result = $conn->query($sql);
+                        echo("<table border=1>");
+                        echo("<th>Id</th>");
+                        echo("<th>Imie</th>");
+                        echo("<th>Dział</th>");
+                        echo("<th>Nazwa_Działu</th>");
+                        echo("<th>Zarobki</th>");
+                        echo("<th>Data_urodzenia</th>");
+                        echo("<th>Wiek</th>");
+                            while($row=$result->fetch_assoc()){ 
+                                echo("<tr>");
+                                    echo("<td>".$row["id_pracownicy"]."</td><td>".$row["imie"]."</td><td>".$row["dzial"]."</td><td>".$row["nazwa_dzial"]."</td><td>".$row["zarobki"]."</td><td>".$row["data_urodzenia"]."</td><td>".$row["wiek"]."</td>");                    echo("</tr>");
+                                echo("</table>");
+                            }
                 ?>
         </body>
 </html>  
