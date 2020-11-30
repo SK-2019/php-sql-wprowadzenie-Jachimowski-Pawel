7b7660b35ed385f28be44cc168e1f73d06e65485
Jachimowski-Pawel <73522925+Jachimowski-Pawel@users.noreply.github.com>
2020-11-30 09:33:23 +0100

    Update dataiczas.php

--- dataiczas.php
+++ dataiczas.php
@@ -10,28 +10,28 @@
             <a href="FAgregat.php">Funkcje Agregujace</a>;
             </div>
                 <?php
-                    function worker($nr_zadania,$z_sql,){
-                    require("connect.php");
-                    $sql=$z_sql
-                    echo("<h2>Tabelka $nr_zadania</h2>".$sql);
-                    $result = $conn->query($sql);
-                        echo("<table border=1>");
-                        echo("<th>Id</th>");
-                        echo("<th>Imie</th>");
-                        echo("<th>Dział</th>");
-                        echo("<th>Nazwa_Działu</th>");
-                        echo("<th>Zarobki</th>");
-                        echo("<th>Data_urodzenia</th>");
-                        echo("<th>Wiek</th>");
-                            while($row=$result->fetch_assoc()){ 
-                                echo("<tr>");
-                                    echo("<td>".$row["id_pracownicy"]."</td><td>".$row["imie"]."</td><td>".$row["dzial"]."</td><td>".$row["nazwa_dzial"]."</td><td>".$row["zarobki"]."</td><td>".$row["data_urodzenia"]."</td><td>".$row["wiek"]."</td>");                    
-                                echo("</tr>");
-                            };
-                        echo("</table>");
-                    };
-                    worker("Zad 1","SELECT *, YEAR(curdate())-YEAR(data_urodzenia) as wiek FROM pracownicy, organizacja WHERE dzial=id_org");
-                    worker("Zad 2",'SELECT *, YEAR(curdate())-YEAR(data_urodzenia) as wiek FROM pracownicy, organizacja WHERE dzial=id_org AND nazwa_dzial="serwis"')    
+                    function worker($nr_zadania,$z_sql){
+                        require("connect.php");
+                        $sql=$z_sql;
+                        echo("<h2>Tabelka $nr_zadania</h2>".$sql);
+                        $result = $conn->query($sql);
+                            echo("<table border=1>");
+                            echo("<th>Id</th>");
+                            echo("<th>Imie</th>");
+                            echo("<th>Dział</th>");
+                            echo("<th>Nazwa_Działu</th>");
+                            echo("<th>Zarobki</th>");
+                            echo("<th>Data_urodzenia</th>");
+                            echo("<th>Wiek</th>");
+                                while($row=$result->fetch_assoc()){ 
+                                    echo("<tr>");
+                                        echo("<td>".$row["id_pracownicy"]."</td><td>".$row["imie"]."</td><td>".$row["dzial"]."</td><td>".$row["nazwa_dzial"]."</td><td>".$row["zarobki"]."</td><td>".$row["data_urodzenia"]."</td><td>".$row["wiek"]."</td>");                    
+                                    echo("</tr>");
+                                };
+                            echo("</table>");
+                        };
+                        worker("Zad 1","SELECT *, YEAR(curdate())-YEAR(data_urodzenia) as wiek FROM pracownicy, organizacja WHERE dzial=id_org");
+                        worker("Zad 2",'SELECT *, YEAR(curdate())-YEAR(data_urodzenia) as wiek FROM pracownicy, organizacja WHERE dzial=id_org AND nazwa_dzial="serwis"');
                 ?>
         </body>
 </html>  
