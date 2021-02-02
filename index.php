8fa955178583d4e11e23ba7451d19b3a73c98598
Jachimowski-Pawel <73522925+Jachimowski-Pawel@users.noreply.github.com>
2021-02-02 09:07:09 +0100

    test index work

--- index.php
+++ index.php
@@ -20,27 +20,28 @@
                 </div>
                 <div class="right">
                     <?php  
-                    function worker($nr_zadania,$z_sql,$polecenie){
-                        require("/assets/connect.php");
-                        $sql=$z_sql;
-                        echo("<h2>Tabelka $nr_zadania</h2>");
-                        echo("<h2>Polecenie: $polecenie </h2>".$sql);
-                        $result=$conn->query($sql);
-                            echo("<table border=1>");
-                            echo("<th>id_pracownicy</th>");
-                            echo("<th>imie</th>");
-                            echo("<th>dzial</th>");
-                            echo("<th>zarobki</th>");
-                                while($row=$result->fetch_assoc()){
-                                    echo("<tr>");
-                                        echo("<td>".$row["id_pracownicy"]."</td><td>".$row["imie"]."</td><td>".$row['dzial']."</td><td>".$row["zarobki"]."</td>");
-                                    echo("</tr>");
-                                };
-                            echo("</table>");
-                    };
-                    worker("Zad 1","SELECT*FROM pracownicy where dzial=2","Pracownicy z dzialu 2");
-                    worker("Zad 2","SELECT*FROM pracownicy where dzial=2 or dzial=3","Pracownicy z dzialu 2 lub 3");
-                    worker("Zad 3","SELECT*FROM pracownicy where zarobki<30","Pracownicy z zarobkami mniejszymi niż 30");
+                    echo("coś");
+                    // function worker($nr_zadania,$z_sql,$polecenie){
+                    //     require("/assets/connect.php");
+                    //     $sql=$z_sql;
+                    //     echo("<h2>Tabelka $nr_zadania</h2>");
+                    //     echo("<h2>Polecenie: $polecenie </h2>".$sql);
+                    //     $result=$conn->query($sql);
+                    //         echo("<table border=1>");
+                    //         echo("<th>id_pracownicy</th>");
+                    //         echo("<th>imie</th>");
+                    //         echo("<th>dzial</th>");
+                    //         echo("<th>zarobki</th>");
+                    //             while($row=$result->fetch_assoc()){
+                    //                 echo("<tr>");
+                    //                     echo("<td>".$row["id_pracownicy"]."</td><td>".$row["imie"]."</td><td>".$row['dzial']."</td><td>".$row["zarobki"]."</td>");
+                    //                 echo("</tr>");
+                    //             };
+                    //         echo("</table>");
+                    // };
+                    // worker("Zad 1","SELECT*FROM pracownicy where dzial=2","Pracownicy z dzialu 2");
+                    // worker("Zad 2","SELECT*FROM pracownicy where dzial=2 or dzial=3","Pracownicy z dzialu 2 lub 3");
+                    // worker("Zad 3","SELECT*FROM pracownicy where zarobki<30","Pracownicy z zarobkami mniejszymi niż 30");
                     ?>
             </div>
         </body>
