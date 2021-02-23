7c39c1cb349c2b7e93f82234eebb6a940df6f46b
Jachimowski-Pawel <73522925+Jachimowski-Pawel@users.noreply.github.com>
2021-02-23 10:06:49 +0100

    finish password_hidden

--- connect.php
+++ connect.php
@@ -1,14 +1,4 @@
 <?php
-// echo("connect.php");
-// $servername="mysql-pawelj.alwaysdata.net";
-// $username="pawelj";
-// $password="Jachimowski0719";
-// $database="pawelj_baza";
-// $conn=new mysqli($servername,$username,$password,$database);
-// if ($conn->connect_error) {
-//     die("Connection failed: " . $conn->connect_error);
-//   }
-// require_once ("config.php");
 $conn=new mysqli($_SERVER['pass'],$_SERVER['userpass'],$_SERVER['password'],$_SERVER['datapass']);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error); 
