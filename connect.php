f111b336e718921bc762d19a590c7f81905bb044
Jachimowski-Pawel <73522925+Jachimowski-Pawel@users.noreply.github.com>
2021-03-05 08:58:57 +0100

    Delete connect.php

--- connect.php
+++
@@ -1,9 +0,0 @@
-<?php
-$servername="mysql-pawelj.alwaysdata.net";
-$username="pawelj";
-$password="Jachimowski0719";
-$database="pawelj_baza";
-$conn=new mysqli($servername,$username,$password,$database);
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-  }
