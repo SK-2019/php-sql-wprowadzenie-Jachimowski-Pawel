bc0c2cb2dd071e90d77aa238f35b5be363e1add7
Jachimowski-Pawel <73522925+Jachimowski-Pawel@users.noreply.github.com>
2021-04-13 09:33:22 +0200

    flex box task
    zadania flexbox uzupełnienie

--- index.php
+++ index.php
@@ -13,7 +13,7 @@
                 <div class="left">
                     <h9>
                     <div class="middle"><a href="https://github.com/SK-2019/php-sql-wprowadzenie-Jachimowski-Pawel">GITHUB</a></div>
-                    <div class="bottom"><a href="/pracownicy/org.php">Organizacja</a></div>
+                    <div class="bottom"><a href="/flex.php">flexbox</a></div>
                     <div class="sub"><a href="/pracownicy/FAgregat.php">Funkcje Agregujace</a></div>
                     <div class="super"><a href="/pracownicy/dataiczas.php">Data i czas</a></div>
                     <div class="text-bottom"><a href="/pracownicy/danedobazy.php">Dane do bazy</a></div>
