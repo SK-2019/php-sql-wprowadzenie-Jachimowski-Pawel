ed4d2eea59cc7c67fe29852f0920d080f29f5947
Jachimowski-Pawel <73522925+Jachimowski-Pawel@users.noreply.github.com>
2021-04-21 11:00:27 +0200

    Menu+header beta

--- index.php
+++ index.php
@@ -8,9 +8,10 @@
         <body>
             <div class="all">
                 <div class="top">
-                    <h8>Paweł Jachimowski</h8>
+                <?php include("header.php") ?>
                 </div>    
                 <div class="left">
+<<<<<<< Updated upstream
                     <h9>
                     <div class="middle"><a href="https://github.com/SK-2019/php-sql-wprowadzenie-Jachimowski-Pawel">GITHUB</a></div>
                     <div class="bottom"><a href="/pracownicy/org.php">Organizacja</a></div>
@@ -19,6 +20,10 @@
                     <div class="text-bottom"><a href="/pracownicy/danedobazy.php">Dane do bazy</a></div>
                     <div class="baseline"><a href="/biblioteka/ksiazki.php">Książki</a></div>
                     </h9>
+=======
+                <?php include("menu.php") ?>
+
+>>>>>>> Stashed changes
                 </div>
                 <div class="right">
                     
